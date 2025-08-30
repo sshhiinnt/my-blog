0476b564361de4a7a5000ace1808fe4b4e91ec63
Shintaro Toichi <rftfaq@gmail.com>
2025-08-31 05:46:26 +0900

    ヘッダー、フッター、スタイル微調整

--- components/footerComp.tsx
+++ components/footerComp.tsx
@@ -7,18 +7,18 @@ const Footer = () => {
     return (
         <footer className="bg-brand text-white">
             <div className=" flex justify-between mx-auto container">
-                <div className="self-center hidden md:block">
+                <div className="self-center hidden md:block sm:px-4">
                     <div className="font-bold text-3xl text-center font-Rockn">YAMAORI</div>
                     <div className="font-Rockn font-bold">山で遊ばせてもらっております</div>
                 </div>
                 <nav>
                     <ul className="list-none py-4">
                         <p className="font-bold">カテゴリー</p>
-                        <li><Link href={`/categories/${encodeURIComponent("登山記録")}`}>登山記録</Link></li>
-                        <li><Link href={`/categories/${encodeURIComponent("トレイルランニング")}`}>トレイルランニング</Link></li>
-                        <li><Link href={`/categories/${encodeURIComponent("クライミング")}`}>クライミング</Link></li>
-                        <li><Link href={`/categories/${encodeURIComponent("道具・装備")}`}>道具・装備</Link></li>
-                        <li><Link href={`/categories/${encodeURIComponent("ひとりごと")}`}>ひとりごと</Link></li>
+                        <li><Link href={`/categories/${encodeURIComponent("登山記録")}`}>-登山記録</Link></li>
+                        <li><Link href={`/categories/${encodeURIComponent("トレイルランニング")}`}>-トレイルランニング</Link></li>
+                        <li><Link href={`/categories/${encodeURIComponent("クライミング")}`}>-クライミング</Link></li>
+                        <li><Link href={`/categories/${encodeURIComponent("道具・装備")}`}>-道具・装備</Link></li>
+                        <li><Link href={`/categories/${encodeURIComponent("ひとりごと")}`}>-ひとりごと</Link></li>
                     </ul>
                 </nav>
                 <nav className="flex flex-col font-bold py-4">

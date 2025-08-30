3a2564b542bd49eff5c8300df4e74150cef5de00
Shintaro Toichi <rftfaq@gmail.com>
2025-08-31 01:57:45 +0900

    yamaori ver_1

--- src/app/api/s3upload/route.ts
+++ src/app/api/s3upload/route.ts
@@ -18,7 +18,7 @@ const bucketName = process.env.AWS_S3_BUCKET_NAME!;
 
 
 console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
-console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY ? "*****" : "undefined");
+console.log("AWS_SECRET_ACCESS_KEY_ID:", process.env.AWS_SECRET_ACCESS_KEY_ID ? "*****" : "undefined");
 console.log("AWS_S3_BUCKET_NAME:", process.env.AWS_S3_BUCKET_NAME);
 console.log('Region:', region);
 

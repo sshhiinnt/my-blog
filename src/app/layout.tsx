57217c0773313dd33c33caa8d2f15d42383ae60a
Shintaro Toichi <rftfaq@gmail.com>
2026-04-01 00:09:10 +0900

    google AdSense実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,8 +53,6 @@ export default function RootLayout({
             gtag('config', 'G-YZH9KCX97E');
           `}
         </Script>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3008954899847844"
-          crossOrigin="anonymous" />
       </head>
       <body className="dark:text-black">
         <Providers>

c7309b28c328ec51e3a676e13d3f2957070ff1a1
Shintaro Toichi <rftfaq@gmail.com>
2026-04-15 00:17:03 +0900

    UI変更

--- components/topImage.tsx
+++ components/topImage.tsx
@@ -19,7 +19,7 @@ const TopImage = () => {
             </div>
             <h3 className="absolute bottom-4 left-4 md:bottom-16 md:left-24 font-noto font-semibold text-xs md:text-2xl tracking-wider">日常に、YAMAORIという選択肢。</h3>
             <h3 className="hidden md:block absolute right-40 top-4 font-serif text-6xl text-text tracking-widest leading-normal [writing-mode:vertical-rl]">生きるには、<br />山や自然との<br />関りから得られる<br />心にくべる薪が<br />必要なんです。</h3>
-            <h3 className="block md:hidden absolute right-16 top-8 font-serif text-xl text-text tracking-widest leading-relaxed [writing-mode:vertical-rl]">生きるには、<br />山や自然との<br />関りから得られる<br />心にくべる薪が<br />必要なんです。</h3>
+            <h3 className="block md:hidden absolute right-16 top-8 font-serif text-sm text-text tracking-widest leading-relaxed [writing-mode:vertical-rl]">生きるには、<br />山や自然との<br />関りから得られる<br />心にくべる薪が<br />必要なんです。</h3>
         </div>
 
     )

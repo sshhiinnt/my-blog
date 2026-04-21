bcdef85b72f2bcde15358fe30bc44e162f653f6e
Shintaro Toichi <rftfaq@gmail.com>
2026-04-21 11:31:43 +0900

    adsスニペット修正

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -53,8 +53,9 @@ export default function RootLayout({
             gtag('config', 'G-YZH9KCX97E');
           `}
         </Script>
-        <script async 
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3008954899847844"
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3008954899847844"
           crossOrigin="anonymous" />
         <meta name="google-adsense-account" content="ca-pub-3008954899847844"></meta>
       </head>
